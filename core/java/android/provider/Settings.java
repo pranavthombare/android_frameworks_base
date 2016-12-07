0bd01a0ce1c4edc0a8ae463e57305cdeaa11c4b2
Pranav Thombare <pranavthombare97@gmail.com>
2016-12-07 21:52:06 +0530

    Fix another conflict

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3729,12 +3729,7 @@ public final class Settings {
          */
         public static final String SCREENSHOT_CROP_BEHAVIOR = "screenshot_crop_behavior";
 
-        /**
-         * Whether or not volume button music controls should be enabled to seek media tracks
-         * @hide
-         */
-        public static final String VOLUME_ROCKER_MUSIC_CONTROLS = "volume_rocker_music_controls";
-
+       
        /**
          * Network traffic indicator, goes from least to greatest significant bitwise
          * 0 = Display up-stream traffic if set
